eb1f5b4274e59f7f2bf479d89f9b25ddd05d9c61
pilarfernandezz <38540218+pilarfernandezz@users.noreply.github.com>
2019-07-12 01:00:30 -0300

    Add files via upload

---
+++ query_login.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const yaml = require('js-yaml');
+const { FileSystemWallet, Gateway } = require('fabric-network');
+const fs = require('fs');
+
+// A wallet stores a collection of identities for use
+const wallet = new FileSystemWallet('./local_fabric_wallet');
+
+async function main(argv) {
+  var myArgs = process.argv.slice(2);
+
+  // A gateway defines the peers used to access Fabric networks
+  const gateway = new Gateway();
+
+  // Main try/catch block
+  try {
+
+    const identityLabel = 'admin';
+    let connectionProfile = yaml.safeLoad(fs.readFileSync('./network.yaml', 'utf8'));
+
+    let connectionOptions = {
+      identity: identityLabel,
+      wallet: wallet,
+      discovery: { enabled:false, asLocalhost: true }
+    };
+
+    // Connect to gateway using network.yaml file and our certificates in _idwallet directory
+    await gateway.connect(connectionProfile, connectionOptions);
+
+    console.log('Connected to Fabric gateway.');
+
+    // Connect to our local fabric
+    const network = await gateway.getNetwork('mychannel');
+
+    console.log('Connected to mychannel. ');
+    
+    const contract = await network.getContract('foodtrack-contract');
+
+    const response = await contract.evaluateTransaction('login', myArgs[0].toString(), myArgs[1].toString());
+
+    // process response
+    console.log('Process buy transaction response.');
+    var str = '[{ "value" : "erro - user null"}]'
+    if(response.value == "erro - user nulo") console.log(JSON.parse(str));
+    console.log( JSON.parse(response) );
+    return response;
+
+
+  } catch (error) {
+    console.log(`******************* Error processing transaction. ${error}`);
+   // console.log(error.stack);
+  } finally {
+    // Disconnect from the gateway
+    console.log('Disconnect from Fabric gateway.');
+    gateway.disconnect();
+  }
+}
+
+// invoke the main function, can catch any error that might escape
+main().then(() => {
+  console.log('done');
+}).catch((e) => {
+  console.log('Final error checking.......');
+  console.log(e);
+  console.log(e.stack);
+  process.exit(-1);
+});
